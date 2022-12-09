df4ec10bb06a4adfd1027b8eacf512945fa46aea
principal kelvo <principalkelvo@gmail.com>
2022-12-10 02:54:08 +0300

    configures the cors for exposure adds comments to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,3 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable,
          :jwt_authenticatable, jwt_revocation_strategy: self
 end
+
+# When a token is dispatched to a user, the JTI claim is taken from the JTI column in the model (which was initialized when the record had been created).
+# At every authenticated action, the incoming token JTI claim is matched against the JTI column for that user. Authentication only succeeds if they are the same.
+# When the user requests to sign out, its JTI column changes, so that the provided token won't be valid anymore.
