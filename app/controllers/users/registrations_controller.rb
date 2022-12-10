4b8fff657d0e4026aae475fcb9d0dca33fe73f3f
principal kelvo <principalkelvo@gmail.com>
2022-12-10 23:34:54 +0300

    adds role on signup

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -3,6 +3,13 @@ class Users::RegistrationsController < Devise::RegistrationsController
 
   private
 
+  def sign_up_params
+    params.require(:user).permit(:email, :password,:password_confirmation, :role)
+  end
+  def account_update_params
+    params.require(:user).permit(:email, :password,:password_confirmation, :current_password, :role)
+  end
+
   def respond_with(resource, options = {})
      if resource.persisted?
       register_success
