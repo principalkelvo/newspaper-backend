4bd0d19bcf371edd387bad4ccd1b8c174e69d63b
principal kelvo <principalkelvo@gmail.com>
2022-12-10 03:27:33 +0300

    secure connection allows only verified users to get data

--- app/channels/application_cable/connection.rb
+++ app/channels/application_cable/connection.rb
@@ -1,4 +1,24 @@
+require 'jwt'
 module ApplicationCable
   class Connection < ActionCable::Connection::Base
+    identified_by :current_user
+
+    def connect
+      self.current_user = find_verified_user
+    end
+
+    private
+
+    def find_verified_user
+      token = request.params[:jwt].split(' ').last
+      decoded_token = JWT.decode(token, ENV['DEVISE_JWT_SECRET_KEY'])
+      if (current_user = User.find_by_jti(decoded_token.first['jti']))
+        current_user
+      else
+        reject_unauthorized_connection
+      end
+    end
   end
 end
+
+# We have created a system that allows us to send information only to verified users while being especially careful not to provide any data of the users through our channels. 
