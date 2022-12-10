8760629a30ed55cfcdf737562c250ec23d35ab70
principal kelvo <principalkelvo@gmail.com>
2022-12-10 03:27:53 +0300

    control users registration

---
+++ app/controllers/users/registrations_controller.rb
@@ -0,0 +1,22 @@
+# app/controllers/users/registrations_controller.rb
+class Users::RegistrationsController < Devise::RegistrationsController
+
+  private
+
+  def respond_with(resource, _opts = {})
+    register_success && return if resource.persisted?
+
+    register_failed
+  end
+
+  def register_success
+    render json: {
+      message: 'Signed up sucessfully.',
+      user: current_user
+    }, status: :ok
+  end
+
+  def register_failed
+    render json: { message: 'Something went wrong.' }, status: :unprocessable_entity
+  end
+end
