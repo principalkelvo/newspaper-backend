17dca20c6ecd29cf502aef77f73ed20ea72eb8a9
principal kelvo <principalkelvo@gmail.com>
2022-12-10 13:08:38 +0300

    add controlers

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,13 @@
 Rails.application.routes.draw do
-  devise_for :users, defaults: { format: :json } 
+  # devise_for :users, defaults: { format: :json } 
+
+  devise_for :users,
+             controllers: {
+               sessions: 'users/sessions',
+               registrations: 'users/registrations'
+             }, defaults: { format: :json } 
+
+             #!signout not working
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
